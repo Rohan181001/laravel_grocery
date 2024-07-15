a001bf799249efd56e9aceb5c31e3cc624426c1c
Rohan181001 <rohanshah181001@gmail.com>
2024-07-15 17:13:22 +0530

    update

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -24,7 +24,7 @@
 								</a>
 							</li>
 							<li class="nav-item">
-								<a href="subcategory.html" class="nav-link">
+								<a href="{{route('sub-categories.index')}}" class="nav-link">
 									<i class="nav-icon fas fa-file-alt"></i>
 									<p>Sub Category</p>
 								</a>
@@ -53,7 +53,7 @@
 							</li>							
 							<li class="nav-item">
 								<a href="orders.html" class="nav-link">
-									<i class="nav-icon fas fa-shopping-bag"></i>
+                                    <i class="nav-icon fas fa-shopping-bag"></i>
 									<p>Orders</p>
 								</a>
 							</li>
