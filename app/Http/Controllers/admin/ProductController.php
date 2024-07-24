3f26aaa70850ba9be1324a073aa65572da50d69d
Rohan181001 <rohanshah181001@gmail.com>
2024-07-24 18:35:34 +0530

    update

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\TempImage;
 use App\Models\ProductImage;
+use App\Models\SubCategory;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -118,6 +119,90 @@ class ProductController extends Controller
       return response()->json([
         'status' => true,
         'message' => 'Product added successfully'
+      ]);
+
+    } else {
+      return response()->json([
+        'status' => false,
+        'errors' => $validator->errors()
+      ]);
+    }
+  }
+
+  public function edit($id, Request $request){
+
+    $product = Product::find($id);
+
+    if(empty($product)){
+       return redirect()->route('products.index')->with('error','Product not found');
+    }
+
+    //Fetch Product Images
+    $productImage = ProductImage::where('product_id',$product->id)->get();
+
+    $subCategories = SubCategory::where('category_id',$product->category_id)->get();
+
+    $data = [];
+    $categories = Category::orderBy('name', 'ASC')->get();
+    // $categories = Brand::orderBy('name','ASC')->get();
+    $data['categories'] = $categories;
+    //  $data['brands'] = $brands;
+    $data['product'] = $product;
+    $data['subCategories'] = $subCategories;
+    $data['productImage'] = $productImage;
+    return view('admin.products.edit',$data);
+  }
+
+  public function update($id, Request $request){
+    
+    $product = Product::find($id);
+
+    $rules = [
+
+      'title' => 'required',
+      'slug' => 'required|unique:products,slug,'.$product->id.',id',
+      'price' => 'required',
+      'sku' => 'required|unique:products,sku,'.$product->id.',id',
+      'track_qty' => 'required|in:Yes,No',
+      'category' => 'required|numeric',
+      'is_featured' => 'required|in:Yes,No',
+
+    ];
+
+    if (!empty($request->track_qty) && $request->track_qty == 'Yes') {
+
+      $rules['qty'] = 'required|numeric';
+    }
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->passes()) {
+
+   
+      $product->title = $request->title;
+      $product->slug = $request->slug;
+      $product->description = $request->description;
+      $product->price = $request->price;
+      $product->compare_price = $request->compare_price;
+      $product->sku = $request->sku;
+      $product->barcode = $request->barcode;
+      $product->track_qty = $request->track_qty;
+      $product->qty = $request->qty;
+      $product->status = $request->status;
+      $product->category_id = $request->category;
+      $product->sub_category_id = $request->sub_category;
+      $product->brand_id = $request->brand;
+      $product->is_featured = $request->is_featured;
+      $product->save();
+
+      //Save Gallery Pics
+      
+
+      $request->session()->flash('success', 'Product updated successfully');
+
+      return response()->json([
+        'status' => true,
+        'message' => 'Product updated successfully'
       ]);
 
     } else {
